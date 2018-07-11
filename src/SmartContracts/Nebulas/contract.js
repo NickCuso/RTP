9130736b2cb41cf83bd8abd2792b79bd5549a591
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-07-11 15:23:58 -0700

    Plus nebulas smart contract.

---
+++ src/SmartContracts/Nebulas/contract.js
@@ -0,0 +1,42 @@
+var Contract = function() 
+{
+  LocalContractStorage.defineMapProperty(this, "topic_to_message"); 
+  LocalContractStorage.defineProperty(this, "owner_addr");
+}
+Contract.prototype = 
+{
+  init: function() 
+  {
+    this.owner_addr = Blockchain.transaction.from;
+  },
+
+  isOwner: function()
+  {
+    return this.owner_addr == Blockchain.transaction.from;
+  },
+  
+  changeOwner: function(new_owner_addr)
+  {
+    assert(this.isOwner(), "This is an owner only call.");
+    assert(new_owner_addr != null, "Missing new address");
+
+    this.owner_addr = new_owner_addr;
+  },
+  setMessage: function(topic, message)
+  {
+    assert(this.isOwner(), "This is an owner only call.");
+    this.topic_to_message.put(topic, message);
+  },
+}
+
+module.exports = Contract
+
+//#region Helpers
+function assert(value, message)
+{
+  if(!value)
+  {
+    throw new Error("Failed assert: " + message);
+  }
+}
+//#endregion
